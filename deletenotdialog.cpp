916bb65d1de92a014a61ccbf59634cf7219537b0
wwwtfden <denn.tdi@gmail.com>
2022-10-13 12:21:36 +0300

    Fixed dialogs

--- deletenotdialog.cpp
+++ deletenotdialog.cpp
@@ -6,9 +6,9 @@ DeleteNotDialog::DeleteNotDialog(QWidget* parent) : QDialog(parent, Qt::WindowTi
     QBoxLayout* baseLayout = new QVBoxLayout;
     QBoxLayout* layout = new QHBoxLayout;
     QPushButton* okButton = new QPushButton("&Ok");
-    connect( okButton, &QAbstractButton::clicked, this, &QDialog::accept );
+    connect(okButton, &QAbstractButton::clicked, this, &QDialog::accept);
     QPushButton* cancelButton = new QPushButton("&Cancel");
-    connect( cancelButton, &QAbstractButton::clicked, this, &QDialog::reject );
+    connect(cancelButton, &QAbstractButton::clicked, this, &QDialog::close);
     QLabel* lbl = new QLabel("Are you sure want to delete this note?");
     //layout->addWidget(lbl);
     layout->addWidget(okButton);
--- saveonclosedialog.cpp
+++ saveonclosedialog.cpp
@@ -6,15 +6,12 @@ SaveOnCloseDialog::SaveOnCloseDialog(QWidget* parent) : QDialog(parent, Qt::Wind
         QBoxLayout* baseLayout = new QVBoxLayout;
         QBoxLayout* layout = new QHBoxLayout;
         QPushButton* okButton = new QPushButton("&Yes");
-        connect( okButton, &QAbstractButton::clicked, this, &QDialog::accept );
+        connect(okButton, &QAbstractButton::clicked, this, &QDialog::accept);
         QPushButton* cancelButton = new QPushButton("&No");
-        connect( cancelButton, &QAbstractButton::clicked, this, &QDialog::reject );
-     //   QPushButton* abortButton = new QPushButton("&Cancel");
-       // connect( cancelButton, &QAbstractButton::clicked, this, &QDialog::);
+        connect(cancelButton, &QAbstractButton::clicked, this, &QDialog::close);
         QLabel* lbl = new QLabel("Save database file?");
         layout->addWidget(okButton);
         layout->addWidget(cancelButton);
-       // layout->addWidget(abortButton);
         baseLayout->addWidget(lbl);
         baseLayout->addLayout(layout);
         setLayout(baseLayout);
